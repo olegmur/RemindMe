e2375157ac97a5a22624a1fc6d5e79fc1b3cc26f
olegmur <olegmur@yandex.ru>
2016-02-23 23:51:58 +0300

    Base project structure ready

---
+++ app/src/main/java/com/mur/remindme/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mur.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by client202b on 23.02.2016.
+ */
+public class MainActivity extends Activity
+
+{
+
+@Override
+ protected void onCreate(Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+}
+
+}
